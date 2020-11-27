2bd3c4033d3cbbf52a867abc45ac05ac0395427e
DAHUO-Melbourne <huodamelbourne@gmail.com>
2020-11-28 02:26:22 +1100

    feat: style added

--- src/component/ImageGallery/ImageGallary.jsx
+++ src/component/ImageGallery/ImageGallary.jsx
@@ -1,17 +1,21 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { actionCreators } from './store'
+import { actionCreators } from './store';
+import './ImageGallary.css';
 
 function ImageGallary (props) {
   const { list, getImages } = props;
   useEffect(() => {
     getImages()
-  }, [])
+  }, [getImages])
   return (
     <div>
-      {list.map(item => (
-        <img src={item} key={item} />
-      ))}
+      <h1 className="heading">Welcome to Pikcha!</h1>
+      <div className="flex-container">
+        {list.map(item => (
+          <img src={item} key={item} alt="img" />
+        ))}
+      </div>
     </div>
   )
 }
