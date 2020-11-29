669b95d93e1c90ff15df8ba0134d00d5bd4e0d97
DAHUO-Melbourne <huodamelbourne@gmail.com>
2020-11-29 13:37:40 +1100

    feat: error reducer test case added

--- src/redux/imageGallery/store.test.js
+++ src/redux/imageGallery/store.test.js
@@ -1,12 +1,18 @@
 import reducer from './reducer';
-import { GET_IMAGE_LIST } from './constants';
+import { GET_IMAGE_LIST, SET_ERROR } from './constants';
 
 describe('reducer correctly returned', () => {
-  it('reducer correctly returned', () => {
+  it('photos list correctly returned', () => {
     const action = {
       type: GET_IMAGE_LIST,
       data: [1, 2, 3]
     }
     expect(reducer({ photos: [] }, action).photos).toStrictEqual([1, 2, 3])
+  })
+  it('error message correctly returned', () => {
+    const action = {
+      type: SET_ERROR,
+    }
+    expect(reducer({ photos: [] }, action).error).toStrictEqual(true)
   })
 })
